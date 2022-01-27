2e9e23ee662b5fd960cdd699e80501f141790f08
Arjun Menon <arjun.umenon@gmail.com>
2022-01-27 20:52:17 +0530

    Added script for adding guest users

--- dev-scripts/add-user-guest.ps1
+++ dev-scripts/add-user-guest.ps1
@@ -4,9 +4,20 @@
 # Connect-AzureAD -TenantDomain $DomainAD
 
 # Get Guest User
-Get-AzureADUser -Filter "UserType eq 'Guest'"
+$GuestUser = Get-AzureADUser -Filter "UserType eq 'Guest'"
+Write-Host $GuestUser
+# Write-Host $GuestUser.UserPrincipalName
 
+# # Adding Users as Guest User
+# $InviteGuestUserEmail = $false
+# $GuestUserList = Import-Csv -Path .\add-user-guest.csv
+
+# Foreach ($GuestUser in $GuestUserList) {
+#     Write-Host "Adding Guest User : $($GuestUser.Name)"
+
+#     New-AzureADMSInvitation -InvitedUserDisplayName $GuestUser.Name -InvitedUserEmailAddress $GuestUser.Email -InviteRedirectURL $GuestUser.RedirectURL -SendInvitationMessage $InviteGuestUserEmail
+# }
 
 # # Remove Guest User
-# $RemoveUserUPN = ""
-# Remove-AzureADUser -ObjectId "<UPN>"
+# $RemoveUserUPN = "alland_M365x538893.onmicrosoft.com#EXT#@aumonline.onmicrosoft.com"
+# Remove-AzureADUser -ObjectId $RemoveUserUPN
