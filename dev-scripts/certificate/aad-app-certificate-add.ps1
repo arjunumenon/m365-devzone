94e662190147418b4edad88fcb0639f064449dcf
Arjun Menon <arjun.umenon@gmail.com>
2022-03-07 20:31:29 +0530

    Working code for adding the Certificate

--- dev-scripts/certificate/aad-app-certificate-add.ps1
+++ dev-scripts/certificate/aad-app-certificate-add.ps1
@@ -1,5 +1,9 @@
-$CertificateValue = Get-PfxCertificate -FilePath ".\AUM Azure DevOps Deployment.pfx"
-$CertificateThumbPrintValue = get-content ".\AUM Azure DevOps Deployment.pfx" -Encoding Byte
-$CertificateValue = [System.Convert]::ToBase64String($CertificateThumbPrintValue)
+$CertificatePath = ".\AUM Azure DevOps Deployment.pfx"
+$AppId = "423dd174-251a-459c-bb1e-c8b766172075"
 
-Write-Host $CertificateValue
+# Get the Certificate String from PFX File
+$EncodedCertificateString = [System.Convert]::ToBase64String(
+    (Get-PfxCertificate -FilePath $CertificatePath).GetRawCertData())
+
+# Adding Certificate using Azure CLI
+az ad app credential reset --id $AppId --cert $EncodedCertificateString --append
