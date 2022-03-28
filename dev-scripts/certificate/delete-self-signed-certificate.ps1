4efa3a52d42d876eff7bd34db2004a35373b2a03
Arjun Menon <arjun.umenon@gmail.com>
2022-03-28 18:48:30 +0530

    Added Delete CErtificate Code

---
+++ dev-scripts/certificate/delete-self-signed-certificate.ps1
@@ -0,0 +1,26 @@
+<#
+.SYNOPSIS
+Deletes already created Certificate Authority.
+.DESCRIPTION
+.EXAMPLE
+.\delete-self-signed-certificate.ps1 -CommonName "MyCert"
+This will delete the certificate from CErttificate Store which has the Name above
+#>
+Param(
+
+   [Parameter(Mandatory=$true)]
+   [string]$CommonName
+)
+
+
+function deleteSelfSignedCertificate{
+    $CertificateList = Get-ChildItem -Path "Cert:\CurrentUser\My" | Where-Object {$_.Subject -Match "$CommonName"} | Select-Object Thumbprint, FriendlyName
+
+    Foreach($Certificate in $CertificateList)
+    {
+        Write-Host "Deleting Certificate with Friendly Name :  '$($Certificate.FriendlyName)' and Thumbprint : '$($Certificate.Thumbprint)'"
+        Remove-Item -Path "Cert:\CurrentUser\My\$($Certificate.Thumbprint)" -DeleteKey
+    }
+}
+
+deleteSelfSignedCertificate
