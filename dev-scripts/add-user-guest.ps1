e07989b171a705992ebf709192772a5c322ebd78
Arjun Menon <arjun.umenon@gmail.com>
2022-05-10 16:27:51 +0530

    Added Guest user addition

--- dev-scripts/add-user-guest.ps1
+++ dev-scripts/add-user-guest.ps1
@@ -1,22 +1,22 @@
 
-# # Connect to Azure AD
-# $DomainAD = "a-um.me"
-# Connect-AzureAD -TenantDomain $DomainAD
+# Connect to Azure AD
+$DomainAD = "a-um.me"
+Connect-AzureAD -TenantDomain $DomainAD
 
 # Get Guest User
 $GuestUser = Get-AzureADUser -Filter "UserType eq 'Guest'"
 Write-Host $GuestUser
-# Write-Host $GuestUser.UserPrincipalName
+Write-Host $GuestUser.UserPrincipalName
 
-# # Adding Users as Guest User
-# $InviteGuestUserEmail = $false
-# $GuestUserList = Import-Csv -Path .\add-user-guest.csv
+# Adding Users as Guest User
+$InviteGuestUserEmail = $false
+$GuestUserList = Import-Csv -Path .\add-user-guest.csv
 
-# Foreach ($GuestUser in $GuestUserList) {
-#     Write-Host "Adding Guest User : $($GuestUser.Name)"
+Foreach ($GuestUser in $GuestUserList) {
+    Write-Host "Adding Guest User : $($GuestUser.Name)"
 
-#     New-AzureADMSInvitation -InvitedUserDisplayName $GuestUser.Name -InvitedUserEmailAddress $GuestUser.Email -InviteRedirectURL $GuestUser.RedirectURL -SendInvitationMessage $InviteGuestUserEmail
-# }
+    New-AzureADMSInvitation -InvitedUserDisplayName $GuestUser.Name -InvitedUserEmailAddress $GuestUser.Email -InviteRedirectURL $GuestUser.RedirectURL -SendInvitationMessage $InviteGuestUserEmail
+}
 
 # # Remove Guest User
 # $RemoveUserUPN = "alland_M365x538893.onmicrosoft.com#EXT#@aumonline.onmicrosoft.com"
