a941ea08675ed10219973934d22ec97e5d657596
Arjun Menon <arjun.umenon@gmail.com>
2022-05-26 12:13:27 +0530

    Updated Azure Resource delete with Count listing

--- dev-scripts/azure-resources-delete.ps1
+++ dev-scripts/azure-resources-delete.ps1
@@ -6,6 +6,8 @@ az configure --defaults group="$ResourceGroupName"
 
 $ResourceList = (az resource list) | ConvertFrom-Json
 
+Write-Host "Total Resource Count is : "$ResourceList.Count
+
 ForEach($Resource in $ResourceList) {
     Write-Host "Resource ID is $($Resource.Id)"
     az resource delete --ids $Resource.Id
