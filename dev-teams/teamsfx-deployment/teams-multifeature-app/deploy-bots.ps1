10256443f0ea28cc810889476325094f39b71480
Arjun Menon <arjun.umenon@gmail.com>
2022-06-09 17:24:30 +0530

    Deploy - Bot - Initial Commit

---
+++ dev-teams/teamsfx-deployment/teams-multifeature-app/deploy-bots.ps1
@@ -0,0 +1,6 @@
+$environmentName = "Prod"
+$resourcegroupName = "Teams-Dev-TCS"
+
+teamsfx deploy bot  --env Prod --interactive false
+
+# teamsfx deploy bot --env Prod --resource-group "Teams-Dev-TCS" --interactive false
