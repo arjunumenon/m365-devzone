d7bd19ef429e96af178f0f8447072c4137a04e1b
Arjun Menon <arjun.umenon@gmail.com>
2022-06-09 17:25:31 +0530

    Deploy - Bot - Minor Refractor

--- dev-teams/teamsfx-deployment/teams-multifeature-app/deploy-bots.ps1
+++ dev-teams/teamsfx-deployment/teams-multifeature-app/deploy-bots.ps1
@@ -1,6 +1,3 @@
 $environmentName = "Prod"
-$resourcegroupName = "Teams-Dev-TCS"
 
-teamsfx deploy bot  --env Prod --interactive false
-
-# teamsfx deploy bot --env Prod --resource-group "Teams-Dev-TCS" --interactive false
+teamsfx deploy bot  --env $environmentName --interactive false
