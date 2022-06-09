15719a22085322c1c581e4afce2d7f5d0aa8ed67
Arjun Menon <arjun.umenon@gmail.com>
2022-06-09 13:27:00 +0530

    Provisioing working with all the Gitignores

--- dev-teams/teamsfx-deployment/teams-multifeature-app/provision-complete.ps1
+++ dev-teams/teamsfx-deployment/teams-multifeature-app/provision-complete.ps1
@@ -1,4 +1,4 @@
 $environmentName = "Prod"
 $resourcegroupName = "Teams-Dev-TCS"
 
-teamsfx provision --env Prod --resource-group "Teams-Dev-TCS"
+teamsfx provision --env Prod --resource-group "Teams-Dev-TCS" --interactive false
