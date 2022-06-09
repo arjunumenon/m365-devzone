2f5481e3c8589f17d63a4a3ba498a40e50e4f485
Arjun Menon <arjun.umenon@gmail.com>
2022-06-09 20:25:18 +0530

    Deploy - Tabs - Working Version

--- dev-teams/teamsfx-deployment/teams-multifeature-app/deploy-tabs.ps1
+++ dev-teams/teamsfx-deployment/teams-multifeature-app/deploy-tabs.ps1
@@ -1,11 +1,27 @@
-$environmentName = "Prod"
+$environmentName = "prod"
 
 # Get App Service ID from Environment variable
 $environmentConfigFilename = ".fx\states\state.$environmentName.json"
-
 $appServiceInformation = Get-Content $environmentConfigFilename | ConvertFrom-Json | Select-Object -Property "fx-resource-frontend-hosting" -ExpandProperty "fx-resource-frontend-hosting"
 $hostName =  $appServiceInformation.domain
 $appServiceInfo = az webapp list --query "[?defaultHostName=='$hostName'].{Id: id}" | ConvertFrom-Json
 Write-Host $appServiceInfo.Id
 
-# # teamsfx deploy function  --env $environmentName --interactive false
+# Going to the Tab Folders in the App Service
+cd tabs
+
+# npm install
+$npmscriptCommand = "build:teamsfx:$environmentName"
+npm run $npmscriptCommand
+
+# Buidling the zip file to deploy to App Service
+$deployFoldername = "build"
+Compress-Archive -Path "$deployFoldername\*" -DestinationPath "$deployFoldername.zip" -Force
+
+# Publishing the deploy folders to Azure App Service
+az webapp deploy --ids $appServiceInfo.Id --src-path "$deployFoldername.zip" --verbose
+
+# Removing the deploy ready folder
+Remove-Item -Recurse -Force "$deployFoldername.zip"
+
+cd ..
