4b05488d4f07b72eacb90b5f212c13e138801dc8
Arjun Menon <arjun.umenon@gmail.com>
2022-10-20 19:20:45 +0530

    Modified Script for deleting the resource groups

--- dev-scripts/azure-resources-delete.ps1
+++ dev-scripts/azure-resources-delete.ps1
@@ -1,14 +1,29 @@
-$ResourceGroupName = "Teams-Dev-RnD"
+<#
+.SYNOPSIS
+Delete all the resources from a specified resource group
+.DESCRIPTION
+.EXAMPLE
+.\azure-resources-delete.ps1 -resourcegroupName "Resourcegroup-to-delete"
+Delete all the resources from a specified resource group
+#>
+Param(
+    [Parameter(Mandatory = $true)]
+    [string]$resourcegroupName
+)
 
-az configure --defaults group="$ResourceGroupName"
+function executeDeleteAzureResources {
+    az configure --defaults group="$resourcegroupName"
 
-# az group list
+    # az group list
 
-$ResourceList = (az resource list) | ConvertFrom-Json
+    $ResourceList = (az resource list) | ConvertFrom-Json
 
-Write-Host "Total Resource Count is : "$ResourceList.Count
+    Write-Host "Total Resource Count is : "$ResourceList.Count
 
-ForEach($Resource in $ResourceList) {
-    Write-Host "Resource ID is $($Resource.Id)"
-    az resource delete --ids $Resource.Id
+    ForEach ($Resource in $ResourceList) {
+        Write-Host "Deleting Resource with ID :  $($Resource.Id)"
+        az resource delete --ids $Resource.Id
+    }
 }
+
+executeDeleteAzureResources
