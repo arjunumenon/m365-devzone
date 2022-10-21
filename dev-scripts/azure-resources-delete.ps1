0c17a9b9845b298626dddedcab0be97edab2f289
Arjun Menon <arjun.umenon@gmail.com>
2022-10-21 19:27:44 +0530

    Refined script further for better

--- dev-scripts/azure-resources-delete.ps1
+++ dev-scripts/azure-resources-delete.ps1
@@ -13,12 +13,18 @@ Param(
 
 function executeDeleteAzureResources {
 
+    # Getting all the resources from specified Resource group. We will be filtering the Resources group using JMESPath Query. 
+    # Refer https://learn.microsoft.com/en-us/cli/azure/query-azure-cli?tabs=concepts%2Cbash for details
     $ResourceList = (az resource list --query "[?resourceGroup=='$resourcegroupName'].{ Name: name, Id: id}") | ConvertFrom-Json
 
-    Write-Host "Total Resource Count is : "$ResourceList.Count
+    # Showing the resource count in the Resource Group
+    # A confirmation prompt will be made to confirm the deletion
+    Write-Host "Total Resource Count in the specified Resource Group is : "$ResourceList.Count
+    Read-Host -Prompt "Press Enter to start deleting (CTRL + C to exit)"
 
     ForEach ($Resource in $ResourceList) {
         Write-Host "Deleting Resource :  $($Resource.Name)"
+        # Deleting the resources with Resource ID
         az resource delete --ids $Resource.Id
     }
 }
