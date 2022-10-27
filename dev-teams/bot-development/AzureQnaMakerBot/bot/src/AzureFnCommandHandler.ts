efa43d79bcb97c1278e1773faf417a65ab984db6
Arjun Menon <arjun.umenon@gmail.com>
2022-10-27 18:31:12 +0530

    Working Azure Function integration

--- dev-teams/bot-development/AzureQnaMakerBot/bot/src/AzureFnCommandHandler.ts
+++ dev-teams/bot-development/AzureQnaMakerBot/bot/src/AzureFnCommandHandler.ts
@@ -6,6 +6,7 @@ import {
     TeamsFxBotSsoCommandHandler,
     TriggerPatterns
 } from "@microsoft/teamsfx"
+import axios from 'axios';
 
 export class AzureFnCommandHandler implements TeamsFxBotSsoCommandHandler {
     triggerPatterns: TriggerPatterns = "azurefn";
@@ -15,17 +16,30 @@ export class AzureFnCommandHandler implements TeamsFxBotSsoCommandHandler {
         message: CommandMessage,
         tokenResponse: TeamsBotSsoPromptTokenResponse
     ): Promise<string | Partial<Activity> | void> {
-        await context.sendActivity("Retrieving user information from Microsoft Graph ...");
+        await context.sendActivity("Invoking the Azure Function...");
 
-        const teamsfx = new TeamsFx();
-        const accessToken = await teamsfx.getCredential().getToken("");
-        // note: empty string argument on the previous line is required for now, this will be fixed in a later release
-        const response = await fetch(`https://azureqnama3a014dapi.azurewebsites.net/api/getQnAResponse`, {
-            headers: {
-                Authorization: `Bearer ${accessToken.token}`,
-            },
-        });
+        const teamsfx  = new TeamsFx().setSsoToken(tokenResponse.ssoToken);
+        // const accessToken = await teamsfx.getCredential().getToken("");
+        // // note: empty string argument on the previous line is required for now, this will be fixed in a later release
+        // const response : any = await fetch(`https://azureqnama3a014dapi.azurewebsites.net/api/getQnAResponse`, {
+        //     headers: {
+        //         Authorization: `Bearer ${accessToken.token}`,
+        //     },
+        // });
 
-        return "Executed Azure Function Command Handler"
+        return await callFunction("getQnAResponse",teamsfx);
     }
+    
 }
+
+async function callFunction(functionName: string, teamsfx?: TeamsFx) {
+    const accessToken = await teamsfx.getCredential().getToken(""); // Get SSO token 
+    // teamsfx.getConfig("apiEndpoint") will read REACT_APP_FUNC_ENDPOINT environment variable 
+    const endpoint = teamsfx.getConfig("apiEndpoint");
+    const response = await axios.get(endpoint + "/api/" + functionName, {
+      headers: {
+        authorization: "Bearer " + accessToken.token,
+      },
+    });
+    return `Value Returned from Azure Function is : ${response.data.userInfoMessage}`;
+  }
