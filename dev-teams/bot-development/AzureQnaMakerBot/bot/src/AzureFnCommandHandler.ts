25cd1f8ee8b51bd3536b16075a7413261f9b0530
Arjun Menon <arjun.umenon@gmail.com>
2022-10-27 18:54:03 +0530

    Minor Refractoring

--- dev-teams/bot-development/AzureQnaMakerBot/bot/src/AzureFnCommandHandler.ts
+++ dev-teams/bot-development/AzureQnaMakerBot/bot/src/AzureFnCommandHandler.ts
@@ -1,45 +1,68 @@
 import { Activity, TurnContext } from "botbuilder"
 import {
-    CommandMessage,
-    TeamsBotSsoPromptTokenResponse,
-    TeamsFx,
-    TeamsFxBotSsoCommandHandler,
-    TriggerPatterns
+  CommandMessage,
+  TeamsBotSsoPromptTokenResponse,
+  TeamsFx,
+  TeamsFxBotSsoCommandHandler,
+  TriggerPatterns
 } from "@microsoft/teamsfx"
 import axios from 'axios';
 
 export class AzureFnCommandHandler implements TeamsFxBotSsoCommandHandler {
-    triggerPatterns: TriggerPatterns = "azurefn";
+  triggerPatterns: TriggerPatterns = "azurefn";
 
-    async handleCommandReceived(
-        context: TurnContext,
-        message: CommandMessage,
-        tokenResponse: TeamsBotSsoPromptTokenResponse
-    ): Promise<string | Partial<Activity> | void> {
-        await context.sendActivity("Invoking the Azure Function...");
+  async handleCommandReceived(
+    context: TurnContext,
+    message: CommandMessage,
+    tokenResponse: TeamsBotSsoPromptTokenResponse
+  ): Promise<string | Partial<Activity> | void> {
+    await context.sendActivity("Invoking the Azure Function...");
+    const teamsfx = new TeamsFx().setSsoToken(tokenResponse.ssoToken);
 
-        const teamsfx  = new TeamsFx().setSsoToken(tokenResponse.ssoToken);
-        // const accessToken = await teamsfx.getCredential().getToken("");
-        // // note: empty string argument on the previous line is required for now, this will be fixed in a later release
-        // const response : any = await fetch(`https://azureqnama3a014dapi.azurewebsites.net/api/getQnAResponse`, {
-        //     headers: {
-        //         Authorization: `Bearer ${accessToken.token}`,
-        //     },
-        // });
+    // Calling the Azure Function for getting the QnA Result
+    const azFunctionResult : any = await callFunction("getQnAResponse", teamsfx);
 
-        return await callFunction("getQnAResponse",teamsfx);
-    }
-    
+    return azFunctionResult;
+  }
+
 }
 
+// Call the Azure Function
 async function callFunction(functionName: string, teamsfx?: TeamsFx) {
-    const accessToken = await teamsfx.getCredential().getToken(""); // Get SSO token 
-    // teamsfx.getConfig("apiEndpoint") will read REACT_APP_FUNC_ENDPOINT environment variable 
-    const endpoint = teamsfx.getConfig("apiEndpoint");
+  const accessToken = await teamsfx.getCredential().getToken(""); // Get SSO token 
+  // teamsfx.getConfig("apiEndpoint") will read REACT_APP_FUNC_ENDPOINT environment variable 
+  const endpoint = teamsfx.getConfig("apiEndpoint");
+  let message: any = null;
+  let funcErrorMsg: string = null;
+  try {
     const response = await axios.get(endpoint + "/api/" + functionName, {
       headers: {
         authorization: "Bearer " + accessToken.token,
       },
     });
-    return `Value Returned from Azure Function is : ${response.data.userInfoMessage}`;
+    message = response.data;
   }
+  catch (err : any) {
+    if (err.response && err.response.status && err.response.status === 404) {
+      funcErrorMsg =
+        'There may be a problem with the deployment of Azure Function App, please deploy Azure Function (Run command palette "TeamsFx - Deploy Package") first before running this App';
+    } else if (err.message === "Network Error") {
+      funcErrorMsg =
+        "Cannot call Azure Function due to network error, please check your network connection status and ";
+      if (err.config.url.indexOf("localhost") >= 0) {
+        funcErrorMsg +=
+          'make sure to start Azure Function locally (Run "npm run start" command inside api folder from terminal) first before running this App';
+      } else {
+        funcErrorMsg +=
+          'make sure to provision and deploy Azure Function (Run command palette "TeamsFx - Provision Resource" and "TeamsFx - Deploy Package") first before running this App';
+      }
+    } else {
+      funcErrorMsg = err.toString();
+      if (err.response?.data?.error) {
+        funcErrorMsg += ": " + err.response.data.error;
+      }
+      alert(funcErrorMsg);
+    }
+  }
+  return message;
+}
