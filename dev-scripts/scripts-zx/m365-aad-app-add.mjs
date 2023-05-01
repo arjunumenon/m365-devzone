1c59ff7551a3109f9f0a72faef01c34406369a2b
Arjun Menon <arjun.umenon@gmail.com>
2023-05-01 21:50:44 +0530

    Modified AAD App Creation

--- dev-scripts/scripts-zx/m365-aad-app-add.mjs
+++ dev-scripts/scripts-zx/m365-aad-app-add.mjs
@@ -4,8 +4,9 @@ async function createAADApp(appName, apiPermissions) {
     console.log(`Creating Azure AD app ${appName}...`);
     let createdApp = null;
     try {
-        createdApp = JSON.parse(await $`m365 aad app add --name ${appName} --apisDelegated ${apiPermissions}  --redirectUris 'https://login.microsoftonline.com/common/oauth2/nativeclient' --platform publicClient --grantAdminConsent --output json`);
-        console.log(`Created Azure AD app ${appName} with id ${app.appId}`);
+        createdApp = JSON.parse(await $`m365 aad app add --name ${appName} --apisDelegated ${apiPermissions}  --redirectUris 'https://login.microsoftonline.com/common/oauth2/nativeclient' --platform spa --grantAdminConsent  --multitenant --withSecret --output json`);
+        // console.log(`Created Azure AD app ${appName} with id ${createdApp.appId}`);
+        // console.log(createdApp);
     }
     catch (err) {
         console.error(`  ${chalk.red(err.stderr)}`);
@@ -16,7 +17,7 @@ const appName = 'AUM Testing App';
 const apiPermissions = 'https://graph.microsoft.com/User.Read';
 
 const appCreated = await createAADApp(appName, apiPermissions);
-if (appCreated == null) {
+if (appCreated === null) {
     console.error(`   ${chalk.red(`Failed to create the App with Name ${appName}`)}`);
 }
 else {
