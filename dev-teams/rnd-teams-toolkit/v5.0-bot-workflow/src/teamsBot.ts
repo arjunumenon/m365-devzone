4c1b23a18ca1415efff4b8ac125428db667376ae
Arjun Menon <1906732@tcsteg.onmicrosoft.com>
2023-08-07 13:42:47 +0530

    Implemented the Welcome Card for installation

--- dev-teams/rnd-teams-toolkit/v5.0-bot-workflow/src/teamsBot.ts
+++ dev-teams/rnd-teams-toolkit/v5.0-bot-workflow/src/teamsBot.ts
@@ -1,9 +1,32 @@
 import { TeamsActivityHandler } from "botbuilder";
+import { aadUtils, welcomeCardUtils } from "./utils/commonUtils";
 
 // An empty teams activity handler.
 // You can add your customization code here to extend your bot logic if needed.
 export class TeamsBot extends TeamsActivityHandler {
   constructor() {
     super();
+
+            // Sends welcome messages to conversation members when they join the conversation.
+        // Messages are only sent to conversation members who aren't the bot.
+        this.onMembersAdded(async (context, next) => {
+          // Iterate over all new members added to the conversation
+          for (const idx in context.activity.membersAdded) {
+              // Greet anyone that was not the target (recipient) of this message.
+              // Since the bot is the recipient for events from the channel,
+              // context.activity.membersAdded === context.activity.recipient.Id indicates the
+              // bot was added to the conversation, and the opposite indicates this is a user.
+              if (context.activity.membersAdded[idx].id !== context.activity.recipient.id) {
+                  //Load the Welcome Card initially
+                  //Setting the Displayname as Undefined for now. Will be updated Later
+                  const userDisplayName = await aadUtils.getDisplayNamefromAADObjectId(context.activity.membersAdded[idx].id);
+                  const card = welcomeCardUtils.generateAndReturnWelcomeCard(userDisplayName)
+                  await context.sendActivity({ attachments: [card] });
+              }
+          }
+
+          // By calling next() you ensure that the next BotHandler is run.
+          await next();
+      });
   }
 }
--- dev-teams/rnd-teams-toolkit/v5.0-bot-workflow/src/utils/commonUtils.ts
+++ dev-teams/rnd-teams-toolkit/v5.0-bot-workflow/src/utils/commonUtils.ts
@@ -6,8 +6,18 @@ export const welcomeCardUtils = {
     generateAndReturnWelcomeCard(userDisplayName: string): Attachment {
         // Render your adaptive card for reply message
         const helpCardData: WelcomeCardData = {
-            initiator: userDisplayName,
+            initiator: userDisplayName!= undefined ? userDisplayName : "User",
         };
         return adaptiveCardsUtils.getWelcomeCard(helpCardData)
+    }
+}
+
+export const aadUtils = {
+    
+    async getDisplayNamefromAADObjectId(aadObjectId: string): Promise<string> {
+
+        //Sending Undefined for now. Will be updated Later
+        return undefined;
+
     }
 }
