01621777242e33d3c65c5f803213af23e8e4a95f
Arjun Menon <arjun.umenon@gmail.com>
2023-12-21 08:12:42 +0530

    Compiling now. Need to integrate it

--- dev-teams/rnd-teams-toolkit/v5.0-completeFeatures/api/src/services/graphServices.ts
+++ dev-teams/rnd-teams-toolkit/v5.0-completeFeatures/api/src/services/graphServices.ts
@@ -1,14 +1,25 @@
 
-import { OnBehalfOfUserCredential, createMicrosoftGraphClientWithCredential } from "@microsoft/teamsfx";
+import { OnBehalfOfUserCredential } from "@microsoft/teamsfx";
+import { Client } from "@microsoft/microsoft-graph-client";
+import { TokenCredentialAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials";
 import { oboAuthConfig } from "../models/authModel";
 
 export async function getUserProfile(accessToken: string): Promise<any> {
+    // Init OnBehalfOfUserCredential instance with SSO token
     const oboCredential = new OnBehalfOfUserCredential(accessToken, oboAuthConfig);
-    // Create a graph client with default scope to access user's Microsoft 365 data after user has consented.
-    const graphClient = createMicrosoftGraphClientWithCredential(
-      oboCredential,
-      [".default"]
-    );
-    const profile: any = await graphClient.api("/me").get();
-    return profile;
+
+    // Create an instance of the TokenCredentialAuthenticationProvider by passing the tokenCredential instance and options to the constructor
+    const authProvider = new TokenCredentialAuthenticationProvider(oboCredential, {
+        scopes: ["User.Read"],
+    });
+
+    // Initialize Graph client instance with authProvider
+    const graphClient = Client.initWithMiddleware({
+        authProvider: authProvider,
+    });
+
+    // Call graph api use `graph` instance to get user profile information
+    const me = await graphClient.api("/me").get();
+
+    return me;
 }
